77fead044bb61a72ea17c2d3aeb2bb52708a15ef
JosepRivaille <josep.de.cid@gmail.com>
2017-05-28 20:41:42 +0200

    Add squares

--- s6/squares.pl
+++ s6/squares.pl
@@ -8,40 +8,42 @@ ejemplo(3,  19, [10,9,7,6,4,4,3,3,3,3,3,2,2,2,1,1,1,1,1,1]).
 ejemplo(4, 112, [50,42,37,35,33,29,27,25,24,19,18,17,16,15,11,9,8,7,6,4,2]).
 ejemplo(5, 175, [81,64,56,55,51,43,39,38,35,33,31,30,29,20,18,16,14,9,8,5,4,3,2,1]).
 
-doExample(E) :- 
+doExample(E) :-
     ejemplo(E, Big, Sides),
     nl, write('Fitting all squares of size '), write(Sides), write(' into big square of size '), write(Big), nl,nl,
-    length(Sides, N), 
+    length(Sides, N),
     length(RowVars, N), % get list of N prolog vars: Row coordinates of each small square
-    length(ColVars, N),
+    length(ColVars, N), % get list of N prolog vars: Col coordinates of each small square
     RowVars ins 1..Big,
     ColVars ins 1..Big,
     insideBigSquare(N, Big, Sides, RowVars),
     insideBigSquare(N, Big, Sides, ColVars),
     nonoverlapping(N, Sides, RowVars, ColVars),
-    append(RowVars,ColVars,Vars),
+    append(RowVars, ColVars, Vars),
     label(Vars),
-    displaySol(N, Sides, RowVars, ColVars), halt.
+    displaySol(N, Sides, RowVars, ColVars).
 
 insideBigSquare(_, _, [], []) :- !.
 insideBigSquare(N, Big, [S|Sides], [V|Vars]) :-
-    V #>= 1, V + S - 1 #=< Big,
-    insideBigSquare(N, Big, Sides, Vars).
+    V + S - 1 #=< Big, insideBigSquare(N, Big, Sides, Vars).
 
 nonoverlapping(1, _, _, _) :- !.
 nonoverlapping(N, [S|Sides], [R|RowVars], [C|ColVars]) :-
-    N1 is N - 1, between(1, N1, I),
-    element(I, RowVars, RP), element(I, ColVars, CP),
-    RP #> R + S #\/ RP #< R #\/ CP #> C + S #\/ CP #< C,
-    nonoverlapping(N1, Sides, RowVars, ColVars).
+    squareRCSnoOverlapsL(S, R, C, Sides, RowVars, ColVars),
+    NP is N - 1, nonoverlapping(NP, Sides, RowVars, ColVars).
 
-displaySol(N, Sides, RowVars, ColVars):- 
+squareRCSnoOverlapsL(_, _, _, [], [], []) :- !.
+squareRCSnoOverlapsL(S, R, C, [SP|Sides], [RP|RowVars], [CP|ColVars]) :-
+    RP #>= R + S #\/ RP + SP #=< R #\/ CP #>= C + S #\/ CP + SP #=< C,
+    squareRCSnoOverlapsL(S, R, C, Sides, RowVars, ColVars).
+
+displaySol(N, Sides, RowVars, ColVars):-
     between(1, N, Row), nl, between(1, N, Col),
-    nth1(K, Sides, S),    
-    nth1(K, RowVars, RV), RVS is RV+S-1, between(RV, RVS, Row),
-    nth1(K, ColVars, CV), CVS is CV+S-1, between(CV, CVS, Col),
+    nth1(K, Sides, S),
+    nth1(K, RowVars, RV), RVS is RV + S - 1, between(RV, RVS, Row),
+    nth1(K, ColVars, CV), CVS is CV + S - 1, between(CV, CVS, Col),
     writeSide(S), fail.
-displaySol(_,_,_,_):- nl,nl,!.
+displaySol(_,_,_,_):- nl.
 
 writeSide(S):- S < 10, write('  '), write(S), !.
 writeSide(S):- write(' '), write(S), !.
